a1783941c07982d02aae66b4b71d83d11d7ec513
ANNMAINAWANGARI <wangariann371@gmail.com>
2023-11-09 14:51:46 +0300

    login and register implementation logic

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -2,12 +2,17 @@
 
 import React ,{useState,FormEvent}from 'react';
 import Auth from '../components/Auth';
+import { authUrl } from '@/utils/network';
+import axios, { AxiosError } from 'axios';
+import { errorHandler } from '@/utils/errorHandler';
+import { useRouter } from 'next/navigation';
 
 type pageProps = {
     
 };
 
 const Login:React.FC<pageProps> = () => {
+    const router = useRouter()
     const [loading, SetLoading] = useState(false);
     const onSubmit =async(
         e: FormEvent<HTMLFormElement>,
@@ -19,6 +24,21 @@ const Login:React.FC<pageProps> = () => {
       email: formRef.current?.email.value,
       password: formRef.current?.password.value,
     };
+    
+      const response = await axios.post(authUrl.login, arg)
+    .catch((e:AxiosError)=>errorHandler(e))
+     SetLoading(false);
+  
+  
+    //   if (response.data) {
+    //     localStorage.setItem(userTokenKey, response.data.token)
+    //     router.push("/");
+    //   }
+
+    if (response) {
+        
+        router.push("/");
+      }
 
 
     }
@@ -31,3 +51,5 @@ const Login:React.FC<pageProps> = () => {
      onSubmit={onSubmit} />;
 }
 export default Login;
+
+
--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -2,12 +2,18 @@
 
 import React ,{useState,FormEvent}from 'react';
 import Auth from '../components/Auth';
+import { authUrl } from '@/utils/network';
+import axios, { AxiosError } from 'axios';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/navigation';
+import { errorHandler } from '@/utils/errorHandler';
 
 type pageProps = {
     
 };
 
 const Register:React.FC<pageProps> = () => {
+    const router = useRouter()
     const [loading, SetLoading] = useState(false);
     const onSubmit =async(
         e: FormEvent<HTMLFormElement>,
@@ -19,6 +25,16 @@ const Register:React.FC<pageProps> = () => {
       email: formRef.current?.email.value,
       password: formRef.current?.password.value,
     };
+    const response = await axios.post(authUrl.register, arg)
+    .catch((e:AxiosError)=>errorHandler(e))
+     SetLoading(false);
+     
+    if (response) {
+      toast("User created successfully", {
+        type: "success",
+      });
+      router.push("/login");
+    }
 
 
     }
